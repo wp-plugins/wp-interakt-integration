212b46ae3fede7c074dd67184b6e2e7dd04d6ef0
peeyush_singla <peeyush_singla@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-08 05:32:54 +0000

    Adding first version of interakt plugin
    git-svn-id: https://plugins.svn.wordpress.org/wp-interakt-integration/trunk@871676 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ interakt_for_wordpress.php
@@ -0,0 +1,163 @@
+<?php // add the admin options page
+/*
+  Plugin Name: Interakt for WordPress
+  Plugin URI: http://interakt.co
+  Description: Integrate the <a href="http://interakt.co">Interakt</a> CRM and messaging app into your WordPress website.
+  Author: Peeyush Singla
+  Author URI: https://twitter.com/peeyush_singla
+  Version: 0.1
+*/
+
+
+class PS_Interakt{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+
+    public $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+      $this->options = get_option( 'interakt_plugin_options_name' );
+      add_action( 'admin_menu', array( $this, 'interakt_plugin_admin_add_page' ) );
+      add_action( 'admin_init', array( $this, 'interakt_plugin_admin_init' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function interakt_plugin_admin_add_page()
+    {
+      // This page will be under "Settings"
+      add_options_page(
+        'Interakt Settings',
+        'Interakt Settings',
+        'manage_options',
+        '__FILE__',
+        array( $this, 'interakt_plugin_options_page' )
+      );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function interakt_plugin_options_page()
+    {
+      // Set class property
+      $this->options = get_option( 'interakt_plugin_options_name' );
+      ?>
+      <div class="wrap">
+        <h2>Configure Interakt App Id</h2>
+        <form method="post" action="options.php">
+          <?php
+            // This prints out all hidden setting fields
+            settings_fields( 'interakt_plugin_options_group' );
+            do_settings_sections( '__FILE__' );
+            submit_button();
+          ?>
+        </form>
+      </div>
+      <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function interakt_plugin_admin_init()
+    {
+      register_setting(
+        'interakt_plugin_options_group', // Option group
+        'interakt_plugin_options_name', // Option name
+        array( $this, 'interakt_plugin_options_validate' ) // Sanitize
+      );
+
+      add_settings_section(
+        'interakt_main_section_id', // ID
+        'App Key Setting', // interakt_app_key
+        array( $this, 'interakt_main_section_cb' ), // Callback
+        '__FILE__' // Page
+      );
+
+      add_settings_field(
+        'interakt_app_key',
+        'Interakt App Id',
+        array( $this, 'interakt_app_key_setting' ),
+        '__FILE__',
+        'interakt_main_section_id'
+      );
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+
+    public function interakt_plugin_options_validate( $input )
+    {
+      return $input;
+    }
+
+    /**
+     * Print the Section text
+     */
+    public function interakt_main_section_cb()
+    {
+      echo '<a href="http://docs.interakt.co">Check Interakt Docs For Help</a>';
+    }
+
+
+    /**
+     * Get the settings option array and print one of its values
+     */
+    public function interakt_app_key_setting()
+    {
+      printf(
+        '<input type="text" id="interakt_app_key" name="interakt_plugin_options_name[interakt_app_key]" size="30" value="%s" />',
+        isset( $this->options['interakt_app_key'] ) ? esc_attr( $this->options['interakt_app_key']) : ''
+      );
+    }
+
+}
+
+
+//Calling constructor method if user is in admin panel
+  if( is_admin() )
+    $my_settings_page = new PS_Interakt();
+
+
+
+//Calling constructor method if user is in front end.
+  add_action('wp_footer', function(){
+    $interakt_object = new PS_Interakt();
+    $interakt_key = ($interakt_object->options['interakt_app_key']);
+    if (!empty($interakt_key)) {
+      echo "<script>
+        (function() {
+        var interakt = document.createElement('script');
+        interakt.type = 'text/javascript'; interakt.async = true;
+        interakt.src = 'http://interakt.co/interakt/$interakt_key.js'
+        var scrpt = document.getElementsByTagName('script')[0];
+        scrpt.parentNode.insertBefore(interakt, scrpt);
+        })()
+      </script>";
+      if ( is_user_logged_in() ) {
+        global $current_user;
+        get_currentuserinfo();
+        $user_name = $current_user->user_login;
+        $email = $current_user->user_email;
+        $created_at = $current_user->user_registered;
+        echo "<script>
+          window.mySettings = {
+          email: '$email',
+          name: '$user_name',
+          created_at: '$created_at',
+          app_id: '$interakt_key'
+          };
+        </script>";
+      }
+    }
+  });
---
+++ readme.txt
@@ -0,0 +1,27 @@
+Interakt-Wordpress-Integration
+Contributors: peeyush singla
+Tags: interakt, lead-capture, interakt.co, crm, feedback form, support, email, feedback, customer relationship management, users
+Requires at least: 3.8
+Tested up to: 3.8.1
+
+Description
+http://interakt.co is a customer relationship management (CRM) and messaging tool for web app owners. WordPress is being widely used as a web app nowadays, so Interakt is an ideal companion app to find out more about your users, contact them, get their instant feedback, and track your relationship with them over time so you can spot those who need attention.
+
+This plugin generates the Javascript install code to integrate all of this functionality into your WordPress-powered web app.
+
+You can also optionally send extra custom data about your users.
+
+
+Installation
+1. Interakt seamlessly integrates with wordpress. wordpress allows you to setup blogs and extend its basic features using plugin and themes. Our plugin allows you to manage customer by providing live support, Chat features to users of your wordpress application.
+2. To integrate your application with Interakt you must have admin access of your application.
+3. First go to your wordpress application admin section e.g:- localhost/wordpress/wp-admin.
+4. Go to plugin section in admin section.
+5. Click on Add New->Upload and upload zipped interakt plugin here.
+6. After upload click on the Activate button.
+7. Post activation there is requirement of adding Interakt app_id in your application so get the app_id by going to Your Interakt account->settings->api_information if don't have one don't worry signup and you can proceed.
+8. Now you got your app_id go to your application admin page->settings->interakt_settings.
+9. Enter your app_id here and your application is integrated with http://interakt.co
+10. Now go to http://interakt.co project and start interakting with users.
+11. If any issue check http://docs.interakt.co blog or contact us at support@interakt.co.
+
